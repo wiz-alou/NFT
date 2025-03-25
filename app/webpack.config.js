8ecbd5a737ad815b37fe402a045a890bff84b680
alassall <alassanesall00765@gmail.com>
2025-03-25 23:02:08 +0000

    ver

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,10 +1,59 @@
-module.exports = function override(config) {
-    // Désactiver complètement l'optimisation pour éviter les problèmes avec terser
-    if (process.env.NODE_ENV === 'production') {
-      config.optimization = {
-        minimize: false
-      };
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify'),
+      url: require.resolve('url'),
+      buffer: require.resolve('buffer'),
+      zlib: require.resolve('browserify-zlib'),
+      path: require.resolve('path-browserify'),
     }
-    
-    return config;
-  };
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    })
+  ]
+};
